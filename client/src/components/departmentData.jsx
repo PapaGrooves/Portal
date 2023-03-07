3b7af62bbecd1ca9c6147f894bb82a4059cc333c
PapaGrooves <kuba10611@gmail.com>
2023-03-07 11:49:12 +0000

    sources images for dashboard cards

--- client/src/components/departmentData.jsx
+++ client/src/components/departmentData.jsx
@@ -7,25 +7,32 @@ export default {
     "2" : {
         name: "MRI",
         id: 2,
+        img: "./images/departments/mri.jpg",
     },
     "3" : {
         name: "Clinics",
         id: 3,
+        img: "./images/departments/clinics.jpeg",
     },
     "4" : {
         name: "Childrens Wings",
         id: 4,
+        img: "./images/departments/childwing.jpg",
     },
     "5" : {
         name: "Wards",
         id: 5,
+        img: "./images/departments/wards.jpg",
     },
     "6" : {
         name: "Surgical Theatres",
         id: 6,
+        img: "./images/departments/theatre.jpg",
+
     },
     "7" : {
         name: "Play Areas",
         id: 7,
+        img: "./images/departments/playarea.jpeg",
     },
 };
